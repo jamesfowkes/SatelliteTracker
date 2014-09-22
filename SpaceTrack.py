fbff88a11e7ef54981693fe7fb5ebb438b9f9edf
James Fowkes <jamesfowkes@gmail.com>
2014-09-22 17:27:40 +0100

    Python 3 updates

--- SpaceTrack.py
+++ SpaceTrack.py
@@ -1,11 +1,12 @@
-import urllib
+import urllib.parse
+import urllib.request
 import datetime
 from TLE import TLE
 
 class SpaceTrack:
 
-    LOGIN_URL = "https://www.space-track.org/ajaxauth/login"
-    QUERY_STRING = "https://www.space-track.org/basicspacedata/query/class/tle_latest/NORAD_CAT_ID/%s/format/3le/limit/1"
+    LOGIN_URL = 'https://www.space-track.org/ajaxauth/login'
+    QUERY_STRING = 'https://www.space-track.org/basicspacedata/query/class/tle_latest/NORAD_CAT_ID/%s/format/3le/limit/1'
 
     def __init__(self, identity, pwd):
 
@@ -14,21 +15,21 @@ class SpaceTrack:
         
     def getTLE(self, norad_cat_id):
     
-        postData = urllib.urlencode(
+        postData = urllib.parse.urlencode(
             {'query':self.QUERY_STRING % norad_cat_id, 
             'identity':self.identity,
             'password':self.pwd}
         )    
         
         try:
-            tle = urllib.urlopen(self.LOGIN_URL, data=postData).read().splitlines()
-            return TLE(datetime.datetime.now(), tle[0], tle[1], tle[2])
+            tle = urllib.request.urlopen(self.LOGIN_URL, data=postData.encode('ascii')).read().splitlines()
+            return TLE(datetime.datetime.now(), tle[0].decode('utf-8'), tle[1].decode('utf-8'), tle[2].decode('utf-8'))
         except:
-            return None
+            raise
 
 def main():
     spaceTrack = SpaceTrack("jamesfowkes@gmail.com", "8rG1eGouVx2aW0X9NzgAj5YDJwaVZO4ciwvmBKi5")
-    print spaceTrack.getTLE("25544")
+    print (spaceTrack.getTLE("25544"))
     
 if __name__ == "__main__":
     main()
